7ef0bb259714b308f7f1d08ee1d7cce89c905ae4
Michael Easter <codetojoy@gmail.com>
2017-08-24 12:43:14 -0300

    configure state

---
+++ src/configure.js
@@ -0,0 +1,19 @@
+
+const R = require('ramda');
+
+function configure(state) {
+    const newPlayers = R.map(R.compose(
+        R.assoc('hand', []),
+        R.assoc('roundsWon', 0),
+        R.assoc('total', 0)
+        ), state.players)
+
+    const newState = R.compose(
+            R.assoc('kitty', []),
+            R.assoc('players', newPlayers)
+        )(state)
+
+    return newState
+}
+
+module.exports = configure 
---
+++ test/configure.js
@@ -0,0 +1,48 @@
+
+const assert = require('assert');
+const configure = require('../src/configure');
+
+describe('configure', function() {
+    const state = {
+        numCards: 16,
+        players: [
+            {name:'Beethoven', strategy: 'popCard'},
+            {name:'Chopin', strategy: 'popCard'},
+            {name:'Mozart', strategy: 'popCard'}
+        ]
+    }
+
+    it('adds kitty', function() {
+        // test
+        const result = configure(state)
+
+        assert.equal(0, result.kitty.length)
+    })
+
+    it('adds player.hand', function() {
+        // test
+        const result = configure(state)
+
+        assert.equal(0, result.players[0].hand.length)
+        assert.equal(0, result.players[1].hand.length)
+        assert.equal(0, result.players[2].hand.length)
+    })
+
+    it('adds player.roundsWon', function() {
+        // test
+        const result = configure(state)
+
+        assert.equal(0, result.players[0].roundsWon)
+        assert.equal(0, result.players[1].roundsWon)
+        assert.equal(0, result.players[2].roundsWon)
+    })
+
+    it('adds player.total', function() {
+        // test
+        const result = configure(state)
+
+        assert.equal(0, result.players[0].total)
+        assert.equal(0, result.players[1].total)
+        assert.equal(0, result.players[2].total)
+    })
+})
