90e3ea410271124ad678a84a757a7130393a2117
Michael Easter <codetojoy@gmail.com>
2017-08-24 15:41:10 -0300

    incremental change as part of a larger frenzy of activity

--- index.js
+++ index.js
@@ -3,15 +3,25 @@ const R = require('ramda')
 const shuffle = require('./src/shuffle')
 const logObject = require('./src/util')
 
-let state = {
+let initState = {
 numCards: 16,
-kitty: [],
 players: [
-    {name:'Beethoven', strategy: 'popCard', roundsWon: 0, total: 0, hand: []},
-    {name:'Chopin', strategy: 'popCard', roundsWon: 0, total: 0, hand: []},
-    {name:'Mozart', strategy: 'popCard', roundsWon: 0, total: 0, hand: []}
+    {name:'Beethoven', strategy: 'popCard'},
+    {name:'Chopin', strategy: 'popCard'},
+    {name:'Mozart', strategy: 'popCard'}
 ]
 };
+
+const newPlayers = R.map(R.compose(
+    R.assoc('hand', []),
+    R.assoc('roundsWon', 0),
+    R.assoc('total', 0)
+    ), initState.players)
+
+const state = R.compose(
+        R.assoc('kitty', []),
+        R.assoc('players', newPlayers)
+    )(initState)
 
 const numPlayers = state.players.length
 const numCardsPerHand = R.divide(state.numCards, R.inc(numPlayers))
