2b92fc6f666872c676d493ef2b804f2dd48a895a
Michael Easter <codetojoy@gmail.com>
2017-08-26 09:39:29 -0300

    new strategies

--- src/strategy.js
+++ src/strategy.js
@@ -1,10 +1,26 @@
 
 const R = require('ramda')
 
+function bidAndNewHand(bid, hand) {
+    const newHand = R.without([bid], hand) 
+    return R.pair(bid, newHand)
+}
+
 function popCard(prizeCard, hand) {
     const bid = R.head(hand)
-    const newHand = R.drop(1, hand)
-    return R.pair(bid, newHand)
+    return bidAndNewHand(bid, hand)
+    // const newHand = R.drop(1, hand)
+    // return R.pair(bid, newHand)
+}
+
+function maxCard(prizeCard, hand) {
+    const bid = Math.max(...hand)
+    return bidAndNewHand(bid, hand)
+}
+
+function minCard(prizeCard, hand) {
+    const bid = Math.min(...hand)
+    return bidAndNewHand(bid, hand)
 }
 
 function applyStrategy(strategy, prizeCard, hand) {
@@ -12,9 +28,13 @@ function applyStrategy(strategy, prizeCard, hand) {
 
     if (strategy == 'popCard') {
         result = popCard(prizeCard, hand)
+    } else if (strategy == 'maxCard') {
+        result = maxCard(prizeCard, hand)
+    } else if (strategy == 'minCard') {
+        result = minCard(prizeCard, hand)
     } 
 
     return result
 }
 
-module.exports.applyStrategy  = applyStrategy 
+module.exports.applyStrategy = applyStrategy 
--- test/strategy.js
+++ test/strategy.js
@@ -3,17 +3,55 @@ const assert = require('assert')
 
 const {applyStrategy} = require('../src/strategy')
 
+/*
+describe('specific strategies', function() {
+    const hand = [19,69,11,18]
+    const prizeCard = 7
+
+    it('maxCard basic', function() {
+        // test
+        const result = maxCard(prizeCard, hand)
+
+        assert.equal(69, result)
+    })
+
+    it('minCard basic', function() {
+        // test
+        const result = minCard(prizeCard, hand)
+
+        assert.equal(11, result)
+    })
+})
+*/
+
 describe('strategies', function() {
     const hand = [19,69,11,18]
+    const prizeCard = 7
 
     it('popCard basic', function() {
-        const prizeCard = 7
-
         // test
         let bid, newHand
-        [bid, newHand] = applyStrategy(prizeCard, 'popCard', hand)
+        [bid, newHand] = applyStrategy('popCard', prizeCard, hand)
 
         assert.equal(19, bid)
+        assert.equal(3, newHand.length)
+    })
+
+    it('maxCard basic', function() {
+        // test
+        let bid, newHand
+        [bid, newHand] = applyStrategy('maxCard', prizeCard, hand)
+
+        assert.equal(69, bid)
+        assert.equal(3, newHand.length)
+    })
+
+    it('minCard basic', function() {
+        // test
+        let bid, newHand
+        [bid, newHand] = applyStrategy('minCard', prizeCard, hand)
+
+        assert.equal(11, bid)
         assert.equal(3, newHand.length)
     })
 })
