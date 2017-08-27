7a2621777ba0952ae58201dbde91dd0fb97d421f
Michael Easter <codetojoy@gmail.com>
2017-08-27 15:02:59 -0300

    R.always is what I had been looking for earlier

--- src/shuffle.js
+++ src/shuffle.js
@@ -3,8 +3,8 @@ const R = require('ramda');
 
 const flip = (x) => Math.floor(Math.random() * x)
 const coinFlip = x => R.equals(0, R.mathMod(flip(x), 2))
-const coinFlipInt = R.ifElse(coinFlip, x => 1, x => -1)
-const mixEmUp = R.ifElse(R.equals(0), x => 0, coinFlipInt)
+const coinFlipInt = R.ifElse(coinFlip, R.always(1), R.always(-1))
+const mixEmUp = R.ifElse(R.equals(0), R.always(0), coinFlipInt)
 
 function shuffle(list) {
     return R.sort(mixEmUp, list)
