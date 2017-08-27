3dff05071907f9e1f45c7d900a4423b746d26266
Michael Easter <codetojoy@gmail.com>
2017-08-27 15:20:42 -0300

    pointfree \!

--- src/shuffle.js
+++ src/shuffle.js
@@ -1,13 +1,12 @@
 
-const R = require('ramda');
+const R = require('ramda')
 
 const flip = (x) => Math.floor(Math.random() * x)
 const coinFlip = x => R.equals(0, R.mathMod(flip(x), 2))
 const coinFlipInt = R.ifElse(coinFlip, R.always(1), R.always(-1))
+
 const mixEmUp = R.ifElse(R.equals(0), R.always(0), coinFlipInt)
 
-function shuffle(list) {
-    return R.sort(mixEmUp, list)
-}
+const shuffle = R.sort(mixEmUp)
 
 module.exports = shuffle 
